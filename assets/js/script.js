f4ec00fb6e8bf3a6a93263e89aa87d531645b24d
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-13 22:20:15 -0500

    save city to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,27 @@ var handleErrors = (response) => {
     }
     return response;
 }
+
+// Function to get and display the current conditions on Open Weather Maps
+var getCurrentConditions = (event) => {
+    // Obtain city name from the search box
+    let city = $('#search-city').val();
+    currentCity= $('#search-city').val();
+    // Set the queryURL to fetch from API using weather search - added units=imperial to fix
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+    fetch(queryURL)
+    .then(handleErrors)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        // Save city to local storage
+        saveCity(city);
+        $('#search-error').text("");
+        // Create icon for the current weather using Open Weather Maps
+        let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        // Offset UTC timezone - using moment.js
+        let currentTimeUTC = response.dt;
+        let currentTimeZoneOffset = response.timezone;
+        let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+        let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
