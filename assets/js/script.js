dfb162f4e910d7ab71109257f5e3514b05dbb2b5
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-13 22:08:40 -0500

    this is an error handle for fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@
 let owmAPI = "788d5638d7c8e354a162d6c9747d1bdf";
 let currentCity = "";
 let lastCity = "";
+
+// this is an error handle for fetch 
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
